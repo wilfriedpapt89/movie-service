ccfcae85451ba22904a64d85daf49882bb109229
wilfried <wilfriedpapt@gmail.com>
2023-10-05 23:35:37 +0200

    adding crossOrigin policy for frontend

--- src/main/java/com/crostsoft/movie/controller/MovieController.java
+++ src/main/java/com/crostsoft/movie/controller/MovieController.java
@@ -5,14 +5,12 @@ import com.crostsoft.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/films")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
 
     private final MovieService movieService;
